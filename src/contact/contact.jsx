451d606cedba569d3edbd640cb2270d8805b913e
jahongirabduazimov707 <jahongirabduazimov707@gmail.com>
2025-01-08 16:09:55 +0500

    Updated

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -5,7 +5,7 @@ import { FaLocationDot } from "react-icons/fa6";
 const Contact = () => {
   return (
     <>
-      <section className="bg-[url('assets/contact.jpg')] min-h-[100vh] bg-no-repeat flex md:items-center w-full bg-center bg-cover">
+      <section className="bg-[url('assets/contact2.jpg')] min-h-[100vh] bg-no-repeat flex md:items-center w-full bg-center bg-cover">
         <div className="fixed top-0 left-0 w-full h-full backdrop-blur-sm bg-black/40" />
         <div className="relative z-30 w-full h-full">
           <div className="w-full max-w-[1350px] px-[15px] mx-auto flex justify-center items-center overflow-hidden">
@@ -31,8 +31,7 @@ const Contact = () => {
                       />
                       <a href="tel:+998946880008">+998 94 688 00 08</a>
                     </div>
-                    <a
-                      href="https://www.google.com/maps/place/Kichik+Xalqa+Yo'li+88,+%D0%A2%D0%BEshkent,+Toshkent,+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD/@41.27028,69.2634689,17.83z/data=!4m5!3m4!1s0x38ae8aa1f68af145:0xcd5946e4ed42be38!8m2!3d41.2703197!4d69.2648999?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwMi4wIKXMDSoASAFQAw%3D%3D"
+                    <p
                       target="_blank"
                       className="flex items-center gap-3 lg:justify-start lg:text-[24px]"
                     >
@@ -41,7 +40,7 @@ const Contact = () => {
                         className="text-[28px] md:text-[36px]"
                       />
                       <p>Малая кольцевая 88Б</p>
-                    </a>
+                    </p>
                   </div>
                 </div>
               </div>
